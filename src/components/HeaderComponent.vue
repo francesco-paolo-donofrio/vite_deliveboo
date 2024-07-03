a06707fb98d44c3fbc6f472331ac54e8445c11e4
Frankosviz <geom.francescopaolo@gmail.com>
2024-07-03 23:35:19 +0200

    creating navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,21 +1,73 @@
 <template>
     <div>
-        <li v-for="(item, index) in menuItems" :key="index">
-            <router-link :to="{ name: item.routeName }" class="nav-link">
-                {{ item.label }}
-            </router-link>
-        </li>
-        <p>ciao</p>
+
+        <header class=" z-1">
+            <nav id="f-d-custom-nav" ref="fdCustomNav"
+                class="fixed-header container f-d-w-80x100 d-flex margin-x-auto rounded-2 align-items-center justify-content-between">
+                <div class="d-flex h-100 align-items-center">
+                    <div class="logo-img-container d-flex align-items-center">
+                        <img class="img-fluid rounded-circle" src="../../public/images/logo_deliveboo.png" alt="logo" />
+                    </div>
+                    <div id="f-d-nav-menu" class="d-flex h-100 d-none d-lg-block align-items-center">
+                        <ul class="navbar-nav h-100 mb-2 mb-lg-0 d-flex flex-row align-items-center">
+                            <li class="p-2" v-for="(item, index) in menuItems" :key="index">
+                                <router-link :to="{ name: item.routeName }" class="nav-link">
+                                    {{ item.label }}
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="user-account gap-3 d-flex fs-3 align-self-stretch align-items-end">
+                    <div id="f-d-nav-login-button" ref="fdNavLoginButton"
+                        class="icon-container rounded-top-2 d-flex flex-column justify-content-center order-2 w-auto"
+                        role="button">
+                        <a href="http://localhost:8000/login"
+                            class="text-decoration-none text-white d-flex flex-column align-items-center">
+                            <i class="fa-solid fa-user"></i>
+                            <span class="small" style="font-size:12px">Area Riservata</span>
+                        </a>
+                    </div>
+                    <div id="f-d-nav-kebab-button" ref="fdNavKebabButton"
+                        class="icon-container rounded-top-2 d-lg-none order-1" role="button"
+                        @click="toggleMenu('hypeNavKebabMenu', $event)">
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+                    <div id="f-d-nav-search-bar-input" ref="fdNavSearchBarInput"
+                        class="search-bar-input bar-input active-color rounded-bottom-2 position-absolute container d-none">
+                        <form class="d-flex p-3" role="search">
+                            <input class="form-control me-4 bg-dark-subtle" type="search" placeholder="Search"
+                                aria-label="Search" />
+                            <button class="mine-custom-btn text-white hover-size" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div id="f-d-nav-kebab-menu" ref="fdNavKebabMenu"
+                        class="bar-input active-color rounded-bottom-2 position-absolute container d-none d-lg-none">
+                        <div class="w-100 d-flex align-content-center justify-content-center p-3">
+                            <ul class="fs-6 w-100 text-decoration-none list-unstyled text-center gap-2 m-0">
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="f-d-nav-login-bar-input" ref="fdNavLoginBarInput"
+                        class="bar-input active-color rounded-bottom-2 position-absolute container d-flex p-0 d-none">
+                        <!-- Authentication and login logic here -->
+                    </div>
+                </div>
+            </nav>
+        </header>
     </div>
 </template>
 
 <script>
-    
-    export default {
-        name: 'HeaderComponent',
-        data(){
-            return {
-                menuItems: [
+
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+
+            show: false,
+            menuItems: [
                 {
                     label: 'Home',
                     routeName: 'home'
@@ -25,11 +77,203 @@
                     routeName: 'restaurant-detail'
                 },
             ],
-            show: false
+        }
+
+    },
+    mounted() {
+        this.fillNavLinks();
+        this.$nextTick(() => {
+            const header = this.$refs.fdCustomNav;
+            if (header) {
+                setTimeout(() => {
+                    header.style.transition = 'transform 0.3s ease-out';
+                }, 200);
+            }
+        });
+        window.addEventListener('scroll', this.updateScrollPosition);
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.updateScrollPosition);
+    },
+    methods: {
+        updateScrollPosition() {
+            const currentScrollPosition = window.scrollY;
+            if (currentScrollPosition !== this.initialPositionWindow || this.openElement !== null) {
+                this.initialPositionWindow = currentScrollPosition;
+                this.render();
+            }
+        },
+        toggleMenu(menuItem, event) {
+            const menuToOpen = this.$refs[menuItem];
+            if (this.openElement === menuToOpen) {
+                menuToOpen.classList.toggle('d-none');
+                this.eventTarget.eventElement.classList.remove('active-color');
+                this.eventTarget.eventElement.innerHTML = this.eventTarget.originalTemplate;
+                this.$refs.fdCustomNav.classList.remove('border-opened-menu');
+                this.openElement = null;
+            } else {
+                this.closeAll();
+                this.openElement = menuToOpen;
+                this.eventTarget.eventElement = event.currentTarget;
+                this.eventTarget.originalTemplate = event.currentTarget.innerHTML;
+                event.currentTarget.classList.add('active-color');
+                event.currentTarget.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+                menuToOpen.classList.toggle('d-none');
+                this.$refs.fdCustomNav.classList.add('border-opened-menu');
+            }
+        },
+        closeAll() {
+            if (this.openElement !== null) {
+                this.openElement.classList.add('d-none');
+            }
+            if (this.eventTarget.eventElement !== null) {
+                this.eventTarget.eventElement.classList.remove('active-color');
+                this.eventTarget.eventElement.innerHTML = this.eventTarget.originalTemplate;
+            }
+        },
+        render() {
+            const renderElement = this.$refs.fdCustomNav;
+            if (renderElement) {
+                renderElement.classList.toggle('window-movement', this.checkMovement());
+            }
+        },
+        checkMovement() {
+            return this.initialPositionWindow !== 0;
+        },
+        fillNavLinks() {
+            this.navLinks = this.$router.options.routes.filter(link => link.meta?.visible ?? false);
+        },
+        capitalizeString(string) {
+            return capitalize(string);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/_variables' as *;
+
+.active-color {
+    background-color: black;
+}
+
+header {
+    height: 75px;
+    background: linear-gradient(to left, $background-tertiary-color, $background-primary-color, $background-fourth-color);
+
+    .active-color {
+        background-color: $background-secondary-color !important;
+        //box-shadow: 2px 2px 5px black;
+    }
+
+    .window-movement {
+        transform: translate(-50%, 10px) !important;
+        transition: transform 0.3s ease-out;
+        box-shadow: 2px 2px 2px $shadow-primary-color;
+    }
+
+    .border-opened-menu {
+        border-bottom-left-radius: 0 !important;
+        border-bottom-right-radius: 0 !important;
+    }
+
+    #f-d-custom-nav {
+        position: fixed;
+        top: 0;
+        z-index: 1000;
+        height: 75px;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: inherit;
+        color: $text-primary-color !important;
+
+        .logo-img-container {
+            width: 7%;
+            width: 90px;
+
+            img {
+                width: 70px;
+                object-fit: cover;
+            }
+        }
+
+        #f-d-nav-menu {
+            .custom-border {
+                border-right: 2px solid $background-tertiary-color;
+            }
+
+            li {
+                position: relative;
+                height: 80%;
+
+                .router-link-exact-active {
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        bottom: 10px;
+                        left: 10%;
+                        width: 80%;
+                        height: 2px;
+                        background-color: $active-primary-color;
+                    }
+                }
+
+                a {
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        bottom: 10px;
+                        left: 10%;
+                        width: 0%;
+                        height: 2px;
+                        background-color: $active-primary-color;
+                        transition: width 0.2s ease-out;
+                    }
+
+                    &:hover::after {
+                        width: 80%;
+                        /* Modifica la larghezza quando passi sopra con il mouse */
+                    }
+                }
+            }
+        }
+
+        .user-account {
+            color: white;
+            background-color: inherit;
+
+            li {
+                &:hover {
+                    background-color: $background-tertiary-color;
+                }
+            }
+
+            .icon-container {
+                width: 40px;
+                height: 57.5px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-bottom: calc((75px - 40px) / 2);
+            }
+
+            .bar-input {
+                top: 100%;
+                left: 0%;
+                background-color: inherit;
+                box-shadow: 2px 2px 2px $shadow-primary-color;
+            }
+
+            .search-bar-input {
+                height: 70px;
+            }
+        }
+
+        button {
+            &:hover {
+                color: white;
+            }
         }
     }
 }
-</script>
-
-
-<style lang="scss" scoped></style>
+</style>
