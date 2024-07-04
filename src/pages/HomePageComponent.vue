267e677f73635edd9ba86e7b1f4e9c1f457d7900
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-07-04 12:23:43 +0200

    first api call

--- src/pages/HomePageComponent.vue
+++ src/pages/HomePageComponent.vue
@@ -1,13 +1,56 @@
 <template>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur architecto aut, doloremque recusandae totam. </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur architecto aut, doloremque recusandae
+        totam. </p>
+    <ul>
+
+        <li v-for="(item, index) in types">{{ item.name }}</li>
+    </ul>
 </template>
 
 <script>
-    export default {
-        name: 'HomePageComponent',
+import { store } from '../store';
+import axios from 'axios';
+
+export default {
+    name: 'HomePageComponent',
+    data() {
+        return {
+            store,
+            types: [],
+            params: {type_id: '1'}
+        }
+    },
+    methods: {
+        getData() {
+            axios.get(this.store.apiBaseUrl + '/restaurants',{params: this.params}).then((res) => {
+                
+                console.log(res.data.results);
+                this.types = res.data.results;
+            });
+        }
+    },
+    mounted() {
+        this.getData();
     }
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped></style>
 
-</style>
+<!-- 
+FUNZIONE DA METTERE NEL CONTROLLER API
+
+public function index(Request $request)
+{
+    if ($request->query('type_id')) {
+        $typeId = $request->query('type_id');
+
+        $restaurants = Restaurant::whereHas('types', function ($query) use ($typeId) {
+        $query->where('type_id', $typeId);
+            })->get();
+
+    return response()->json(['results' => $restaurants]);
+    }
+}
+
+-->
