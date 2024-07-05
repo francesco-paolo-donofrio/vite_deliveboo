a3aceec28277182008c6ce7527de3ce990696ccd
danielbosa <danielbosa95@gmail.com>
2024-07-05 22:05:52 +0200

    Deleted description. Added types from api

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,17 +1,13 @@
 <template>
-  <section>
-<div class="card">
-  <img class="card-img-top" :src="getImage" :alt="item.name">
-  <div class="card-body">
-    <h5 class="card-title">{{ item.name }}</h5>
-    <small class="card-text" v-for="(type, index) in item.types" :key="index">{{ type.name }}<span v-if="index < item.types.length - 1">, </span>
-    </small>
-    <div v-if="item.description">
-        <p class="card-text" v-html="item.description.substring(0, 40) + '...'"></p>
+<section>
+    <div class="card">
+        <img class="card-img-top" :src="getImage" :alt="item.name">
+        <div class="card-body">
+            <h5 class="card-title">{{ item.name }}</h5>
+            <small class="card-text" v-for="(type, index) in item.types" :key="index">{{ type.name }}<span v-if="index < item.types.length - 1">, </span></small>
+        </div>
+        <RouterLink :to="{ name: 'restaurant-detail', params: {'id': item}}" class="btn btn-success" @click="getid(item)">Visualizza Ristorante</RouterLink>
     </div>
-  </div>
-  <RouterLink :to="{ name: 'restaurant-detail', params: {'id': item}}" class="btn btn-success" @click="getid(item)">Visualizza Ristorante</RouterLink>
-</div>
     </section> 
 </template>
 
@@ -28,7 +24,7 @@ import { store } from '../store';
         methods:{
             getid(item){
                 console.log(item);
-               this.store.item= item.id
+                this.store.item= item.id
             }
         },
         mounted(){
@@ -36,8 +32,7 @@ import { store } from '../store';
         },
         computed: {
             getImage() {
-                return this.item.image ? store.imgBasePath + this.item.image : '/images/placeholder.png';
-                
+                return this.item.image ? store.imgBasePath + this.item.image : '/images/placeholder.png';      
             }
             
         }
