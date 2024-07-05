30263cd825b8289001583ced05ea85bf9973c53f
danielbosa <danielbosa95@gmail.com>
2024-07-05 22:52:02 +0200

    If no results from searching by types, error message show types selected

--- src/pages/HomePageComponent.vue
+++ src/pages/HomePageComponent.vue
@@ -12,13 +12,17 @@
         <div class="row">
             <div class="col-12 col-md-3 col-sm-6 mt-4" v-for="(item, index) in restaurants">
                 <div class="row">
-                    <!-- SE LA RICERCA NON DÁ NESSUN RISULTATO -->
-                    <div class="col-12 col-lg-6" v-if="item.length < 1">
-                        <h3>Nessun post trovato per la tipologia: ... </h3>
-                        <!-- inserire qui la funzione che ritorna il nome della tipologia -->
-                    </div>
                     <CardComponent :item="item" />
                 </div>
+            </div>
+            <!-- SE LA RICERCA NON DÁ NESSUN RISULTATO -->
+            <div class="col-12 col-lg-6" v-if="restaurants.length == 0">
+                <h3>Nessun ristorante trovato che soddisfi questi requisiti:
+                    <ul>
+                        <li v-for="(name, index) in selectedtypesNames" :key="index">{{ name }}</li>
+                    </ul>
+                </h3>
+                <!-- inserire qui la funzione che ritorna il nome della tipologia -->
             </div>
         </div>
     </div>
@@ -46,28 +50,25 @@ export default {
     data() {
         return {
             store,
-            params: new URLSearchParams(),
-            //params: {},           
+            params: new URLSearchParams(),          
             types: [],
             restaurants: [],
             selectedtypes: [],
+            selectedtypesNames: [],
         }
     },
     methods: {
         getTypes() {
             axios.get(this.store.apiBaseUrl + '/types').then((res) => {
                 console.log(res.data.results);
-                console.log(this.$route.params, 'prova');
+                //console.log(this.$route.params, 'prova');
                 this.types = res.data.results;
             });
         },
         getRestaurants() {
-            //this.params.append("type_id", prova);
-            //this.params.append("type_id", 3);
-
             axios.get(this.store.apiBaseUrl + '/restaurants', { params: this.params }).then((res) => {
-                console.log(this.params);
-                console.log(res.data.results);
+                //console.log(this.params);
+                //console.log(res.data.results);
                 this.restaurants = res.data.results;
             });
         },
@@ -76,10 +77,13 @@ export default {
             if (this.selectedtypes.includes(item.id)) {
                 const index = this.selectedtypes.indexOf(item.id);
                 this.selectedtypes.splice(index, 1);
+                this.selectedtypesNames.splice(index, 1);
             } else {
                 this.selectedtypes.push(item.id);
+                console.log(item.name);
+                this.selectedtypesNames.push(item.name);
             }
-            console.log(this.selectedtypes);
+            //console.log(this.selectedtypes);
         },
         sendTypes() {
             //console.log(this.selectedtypes);
@@ -87,12 +91,12 @@ export default {
                 this.params.append("type_id", element);
             })
             //this.params.append("type_id", this.selectedtypes);
-            console.log(this.params);
+            //console.log(this.params);
             axios.get(this.store.apiBaseUrl + '/restaurants', { params: this.params }).then((res) => {
-                console.log(this.params);
-                console.log(res.data.results);
+                //console.log(this.params);
+                //console.log(res.data.results);
                 this.restaurants = res.data.results;
-
+                console.log(res.data.results);
             });
             this.params = new URLSearchParams();
         },
@@ -124,35 +128,3 @@ export default {
 }
 
 </style>
-
-
-<!-- 
-let url = window.location.href;
-let params = new URLSearchParams(url.search);
-
-//Add a new parameter.
-params.append("foo", 4);
-//Query string is now: 'foo=4'
--->
-
-<!-- 
-updateUrl() {
-      // Ottieni l'URL attuale
-        const currentUrl = this.$route.fullPath;
-        console.log('URL attuale:', currentUrl);
-
-      // Crea un nuovo oggetto URLSearchParams con i nuovi parametri
-        const searchParams = new URLSearchParams(this.params);
-
-      // Aggiorna l'URL senza ricaricare la pagina
-        this.$router.push({ path: this.$route.path, query: Object.fromEntries(searchParams) });
-    }
-}
-
-let url = window.location.href;
-           
-            let paramsTest = new URLSearchParams(url.search);
-            //Add a new parameter.
-            paramsTest.append("type_id", 3);
-            //Query string is now: 'foo=4'
--->
